927380d08918247766cbb7cc083796182f8c92df
Davide Alberani <da@erlug.linux.it>
2017-06-20 00:29:20 +0200

    symlink to avoid modifications to the wrong file

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp libs/head.min.js reveal.js/js/
-cp index.html git-crash-course.md reveal.js/
+rm -f reveal.js/index.html
+ln index.html git-crash-course.md reveal.js/ 2> /dev/null
 cd reveal.js
 npm install
 npm start
