7b35f5b47c6b736e6c2b8ac84b768a17c7aef660
Davide Alberani <d.alberani@endian.com>
2017-07-29 15:32:28 +0200

    update installation script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git submodule init
+git submodule update 2> /dev/null
+mkdir -p reveal.js/js/
 cp libs/head.min.js reveal.js/js/
 rm -f reveal.js/index.html
 ln index.html git-crash-course.md reveal.js/ 2> /dev/null
