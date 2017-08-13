6c40a5f15c96a3f40bb2e5b1dcdb1c616fac5959
Davide Alberani <da@erlug.linux.it>
2017-08-13 20:22:33 +0200

    support for images

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 git submodule init
 git submodule update 2> /dev/null
 mkdir -p reveal.js/js/
-cp libs/head.min.js reveal.js/js/
+cp js/* reveal.js/js/
+cp css/* reveal.js/css/
+cp images/* reveal.js/images/
 rm -f reveal.js/index.html
 ln index.html git-crash-course.md reveal.js/ 2> /dev/null
 cd reveal.js
